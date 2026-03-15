a6b789c6b3fbbd433698abfea59cfe1f9bebd3c8
rezy <rezac.martin@icloud.com>
2026-03-15 22:20:01 +0100

    Initial Commit

---
+++ AppHomekit/AppHomekitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppHomekitApp.swift
+//  AppHomekit
+//
+//  Created by Martin Řezáč on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct AppHomekitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
